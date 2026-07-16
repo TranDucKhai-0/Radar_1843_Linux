625b3ffa7581eee3e504b9347aeec2b9b4ad0cc1
Khai <khaixayda@gmail.com>
2026-07-16 09:35:50 +0700

    refactory: cấu trúc lại struct out data thành header riêng

--- out_of_box_1843_mss/mss_main.c
+++ out_of_box_1843_mss/mss_main.c
@@ -636,6 +636,10 @@
 /* Profiler Include Files */
 #include <ti/utils/cycleprofiler/cycle_profiler.h>
 
+/* Include USER library*/
+#include "../component/include/out_type_data.h"
+/* End include USER library*/
+
 #ifdef SYS_COMMON_RTRIM_MODIFY_EN
 #include <ti/utils/rtrim/rtrimutils.h>
 #endif
@@ -651,23 +655,6 @@ Semaphore_Handle uartTxSemHandle;
 /* Biến toàn cục lưu trữ dữ liệu GTrack để đẩy ra ngoài */
 #define FIXED_PACKET_SIZE 260
 #define MAX_CUSTOM_TARGETS 7
-
-typedef struct {
-    uint8_t  magicWord[4];  // 4 Bytes: Chuỗi báo hiệu đồng bộ (0x01, 0x02, 0x03, 0x04)
-    uint32_t numTargets;    // 4 Bytes: Số lượng đối tượng hợp lệ trong payload (0 - 7)
-} Custom_Output_Header;
-
-typedef struct {
-    uint32_t tid;           // 4 Bytes: Target ID
-    float    posX;          // 4 Bytes: Tọa độ tâm X (m)
-    float    posY;          // 4 Bytes: Tọa độ tâm Y (m)
-    float    posZ;          // 4 Bytes: Tọa độ tâm Z (m)
-    float    velX;          // 4 Bytes: Vận tốc X (m/s)
-    float    velY;          // 4 Bytes: Vận tốc Y (m/s)
-    float    velZ;          // 4 Bytes: Vận tốc Z (m/s)
-    float    dimX;          // 4 Bytes: Kích thước - Độ rộng (m)
-    float    dimY;          // 4 Bytes: Kích thước - Độ cao (m)
-} Custom_Target_Data;
 
 /* Bộ đệm cố định 260 Bytes để gửi ra ngoài */
 uint8_t gUartTxBuffer[FIXED_PACKET_SIZE];
